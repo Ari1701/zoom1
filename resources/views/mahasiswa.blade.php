73145022860c5242f8b8c4d3b8d569a280f5da15
ari <aryfarah45@gmail.com>
2024-02-20 20:25:28 +0700

    Data Diri

---
+++ resources/views/mahasiswa.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+
+<head>
+
+
+<meta charset="UTF-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Data Diri</title>
+</head>
+<body>
+<ol>
+
+<h1>Data Diri</h1>
+
+<li>Nama: Ari Farah</li>
+<li>NIM: L200210108</li>
+<li>Alamat: Pati</li>
+<li>Hobi: Futsal</li>
+
+
+</ol>
+
+ </body>
+
+ </html>
